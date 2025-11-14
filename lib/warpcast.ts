67af5c8f37eb17130dab07f226e17b0639362d8b
Graven Prest <15019279+gravenp@users.noreply.github.com>
2025-11-14 16:16:36 -0700

    adds beamr branding and media

--- lib/warpcast.ts
+++ lib/warpcast.ts
@@ -7,7 +7,7 @@ import { env } from "@/lib/env";
  */
 export async function getFarcasterManifest() {
   // let frameName = "BEAMR";
-  let frameName = "RMAEB";
+  let frameName = "Beamr";
   let noindex = false;
   const appUrl = env.NEXT_PUBLIC_URL;
   if (appUrl.includes("localhost")) {
@@ -32,29 +32,29 @@ export async function getFarcasterManifest() {
       iconUrl: `${appUrl}/images/icon.png`,
       homeUrl: appUrl,
       imageUrl: `${appUrl}/images/feed.png`,
-      buttonTitle: `Launch App`,
+      buttonTitle: `Start Beamr`,
       splashImageUrl: `${appUrl}/images/splash.png`,
-      splashBackgroundColor: "#000000",
+      splashBackgroundColor: "#0F0E0E",
       webhookUrl: `${appUrl}/api/webhook`,
       // Metadata https://github.com/farcasterxyz/miniapps/discussions/191
       // subtitle: "BEAMR", // 30 characters, no emojis or special characters, short description under app name
-      subtitle: "RMAEB", // 30 characters, no emojis or special characters, short description under app name
-      description: "RMAEB", // 170 characters, no emojis or special characters, promotional message displayed on Mini App Page
+      subtitle: "Dynamic micro-subscriptions", // 30 characters, no emojis or special characters, short description under app name
+      description: "The easiest way to sustainably reward people who make your feed worth scrolling.", // 170 characters, no emojis or special characters, promotional message displayed on Mini App Page
       // description: "BEAMR", // 170 characters, no emojis or special characters, promotional message displayed on Mini App Page
       primaryCategory: "social",
-      tags: ["mini-app"], // up to 5 tags, filtering/search tags
+      tags: ["mini-app", "base", "creator", "utility", "streaming"], // up to 5 tags, filtering/search tags
       // tagline: "BEAMR", // 30 characters, marketing tagline should be punchy and descriptive
-      tagline: "RMAEB", // 30 characters, marketing tagline should be punchy and descriptive
+      tagline: "Dynamic micro-subscriptions", // 30 characters, marketing tagline should be punchy and descriptive
       ogTitle: `${frameName}`, // 30 characters, app name + short tag, Title case, no emojis
       // ogDescription: "BEAMR", // 100 characters, summarize core benefits in 1-2 lines
-      ogDescription: "RMAEB", // 100 characters, summarize core benefits in 1-2 lines
+      ogDescription: "The easiest way to sustainably reward people who make your feed worth scrolling.B", // 100 characters, summarize core benefits in 1-2 lines
       screenshotUrls: [
         // 1284 x 2778, visual previews of the app, max 3 screenshots
         `${appUrl}/images/feed.png`,
       ],
       heroImageUrl: `${appUrl}/images/feed.png`, // 1200 x 630px (1.91:1), promotional display image on top of the mini app store
       ogImageUrl: `${appUrl}/images/feed.png`, // 1200 x 630px (1.91:1), promotional image, same as app hero image
-      noindex: noindex,
+      noindex: true,
     },
     "baseBuilder": {
     "ownerAddress": "0x375567484B27648C7CE609425043119b3c0A7285"
