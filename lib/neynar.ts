e90316de6c9391a788483de9287de9d19eb69b10
jordan <jordan.lesich@gmail.com>
2025-11-21 11:06:16 -0800

    cache check for single user fetch

--- lib/neynar.ts
+++ lib/neynar.ts
@@ -1,5 +1,10 @@
 import { env } from '@/lib/env';
-import { getNeynarUsers, setNeynarUsers } from './redis';
+import {
+  getNeynarUser,
+  getNeynarUsers,
+  setNeynarUser,
+  setNeynarUsers,
+} from './redis';
 import z from 'zod';
 
 export const NeynarUserSchema = z.object({
@@ -22,7 +27,17 @@ export const NeynarUserSchema = z.object({
 
 export type NeynarUser = z.infer<typeof NeynarUserSchema>;
 
-export const fetchUser = async (fid: string): Promise<NeynarUser> => {
+export const fetchUser = async (
+  fid: string,
+  opts?: { fresh?: boolean }
+): Promise<NeynarUser> => {
+  const { fresh } = opts ?? {};
+
+  if (!fresh) {
+    const cached = await getNeynarUser(fid);
+    if (cached) return cached;
+  }
+
   const response = await fetch(
     `https://api.neynar.com/v2/farcaster/user/bulk?fids=${fid}`,
     {
@@ -31,6 +46,7 @@ export const fetchUser = async (fid: string): Promise<NeynarUser> => {
       },
     }
   );
+
   if (!response.ok) {
     console.error(
       'Failed to fetch Farcaster user on Neynar',
@@ -38,8 +54,13 @@ export const fetchUser = async (fid: string): Promise<NeynarUser> => {
     );
     throw new Error('Failed to fetch Farcaster user on Neynar');
   }
+
   const data = await response.json();
-  return data.users[0];
+  const user = data.users[0] as NeynarUser;
+
+  await setNeynarUser(fid, user);
+
+  return user;
 };
 
 // export const fetchUsersByEthAddress = async (
@@ -144,9 +165,6 @@ export const fetchUsers = async (fids: number[]): Promise<NeynarUser[]> => {
         console.log();
       }
     });
-
-    // log out missing fids
-    console.log('Missing FIDs from cache:', missingFids);
 
     if (missingFids.length) {
       const res = await fetch(
